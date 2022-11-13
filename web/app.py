9f07187210845f2ab68779ed8b79cd80317bea9e
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-14 01:30:58 +0800

    add full code

---
+++ web/app.py
@@ -0,0 +1,97 @@
+from flask import Flask, Response, request
+from flask import render_template, redirect
+from pythonosc.udp_client import SimpleUDPClient
+
+
+# def send_osc_msg():
+
+#     ip = "127.0.0.1"
+#     port = 1337
+#     client = SimpleUDPClient(ip, port)  # Create client
+#     client.send_message("/something/address", 123)   # Send float message
+
+# def send_osc_msg_a():
+
+#     ip = "127.0.0.1"
+#     port = 3819
+#     client = SimpleUDPClient(ip, port)  # Create client
+#     client.send_message("/toggle_roll", 0)   # Send float message
+
+
+app = Flask(__name__)
+
+
+@app.route('/player')
+def player():
+
+    #page = request.args.get('page', default = 1, type = int)
+    set_name = request.args.get('set', default = '*', type = str)
+    print(set_name)
+
+    return render_template('player.html', set_name = set_name) 
+@app.route('/menu')
+def menu():
+
+    return render_template('menu.html')
+
+@app.route('/longbow')
+def longbow():
+
+    return render_template('longbow.html')
+
+@app.route('/vibrato')
+def vibrato():
+
+    return render_template('vibrato.html')
+
+@app.route('/basic01')
+def basic01():
+
+    return render_template('basic01.html')
+
+@app.route('/basic02')
+def basic02():
+
+    return render_template('basic02.html')
+
+
+@app.route('/basic03')
+def basic03():
+
+    return render_template('basic03.html')
+
+'''
+@app.route('/player.html')
+def player():
+    return render_template('player.html') 
+'''
+# @app.route('/send_osc')
+# def send_osc():
+#     send_osc_msg()
+#     return redirect('/')
+
+# @app.route('/send_osc_a')
+# def send_osc_a():
+#     send_osc_msg_a()
+#     return redirect('/')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route("/wav")
+def streamwav():
+    def generate():
+        with open("static/audio/session.wav", "rb") as fwav:
+            data = fwav.read(1024)
+            while data:
+                yield data
+                data = fwav.read(1024)
+    return Response(generate(), mimetype="audio/x-wav")
+
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run('0.0.0.0')
